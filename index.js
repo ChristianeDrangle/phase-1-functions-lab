5ee7252a6f3de1a5b3df7a1e8e7064c776de141d
Christiane Drangle <positivitee@proton.me>
2023-04-03 17:32:47 -0400

    Functions Lab passing

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    const headquarters = 42;
+    return Math.abs(blocks - headquarters);
+}
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264
+}
+function distanceTravelledInFeet(starting, ending) {
+    return Math.abs(starting - ending) * 264;
+}
+function calculatesFarePrice(starting, ending) {
+    const distance = distanceTravelledInFeet(starting, ending)
+
+    if (distance <= 400) {
+        return 0
+    }
+    else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * .02
+    }
+    else if (distance > 2000 && distance <= 2500) {
+        return 25.00
+    }
+    else if (distance > 2500) {
+        return 'cannot travel that far'
+    }
+
+
+
+}
